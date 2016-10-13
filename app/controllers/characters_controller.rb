8d19b96d0f286278db0c2adf61fcbb823df667cc
manish pradhan <pradhanmanish33@gmail.com>
2016-10-13 12:26:34 -0400

    commit

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,19 @@
+class CharactersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+end
+end
---
+++ app/controllers/houses_controller.rb
@@ -0,0 +1,18 @@
+class HousesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+end

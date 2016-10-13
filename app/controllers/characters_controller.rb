4de25c047040b074707342a3acb50e6dd9b3a036
manish pradhan <pradhanmanish33@gmail.com>
2016-10-13 14:34:40 -0400

    commit

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,19 +1,19 @@
 class CharactersController < ApplicationController
 
   def index
-
+    @characters = Character.all
   end
 
   def new
-
+    @character = Character.new
   end
 
   def show
+    @character = Character.find(params[:id])
 
   end
 
   def create
 
   end
-end
 end
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,15 +1,15 @@
 class HousesController < ApplicationController
 
   def index
-
+      @houses = House.all
   end
 
   def new
-
+    @house = House.new
   end
 
   def show
-
+    @house = House.find(params[:id])
   end
 
   def create
